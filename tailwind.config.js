8e8abd0c65482c330fa7bb3f735a78e2badcb938
Hamsa Abdirashid <xamsze1234@gmail.com>
2022-02-14 19:16:32 +0300

    the solution is know using react, instead of vanilla HTML and CSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    content: ['./*.{js, jsx, css}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Rubik', 'Segoe UI', 'sans-serif'],
+            },
+            screens: {
+                xs: '375px',
+            },
+        },
+    },
+    plugins: [],
+}
