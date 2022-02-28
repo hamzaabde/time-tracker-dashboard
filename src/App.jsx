48ac2ebc3b3a4a8488a2b10facce3d21a6ad9f9a
hamzaabde <xamsze1234@gmail.com>
2022-02-28 23:50:29 +0300

    desktop version finished

--- src/App.jsx
+++
@@ -1,7 +0,0 @@
-export default function () {
-    return (
-        <div className="h-screen bg-black">
-            <h1 className="text-blue text-9xl">Hello World!</h1>
-        </div>
-    )
-}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,29 @@
 module.exports = {
-    content: ['./*.{js, jsx, css}'],
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Rubik', 'Segoe UI', 'sans-serif'],
-            },
-            screens: {
-                xs: '375px',
-            },
-        },
-    },
-    plugins: [],
+	content: ['index.html'],
+	theme: {
+		fontFamily: {
+			sans: ['Rubik', 'Segoe UI', 'sans-serif'],
+		},
+		extend: {
+			screens: {
+				desktop: '1440px',
+				mobile: '375px',
+			},
+			colors: {
+				'primary-blue': 'hsl(246, 80%, 60%)',
+				'light-orange': 'hsl(15, 100%, 70%)',
+				'soft-blue': 'hsl(195, 74%, 62%)',
+				'light-red': 'hsl(348, 100%, 68%)',
+				'lime-green': 'hsl(145, 58%, 55%)',
+				'dark-purple': 'hsl(264, 64%, 52%)',
+				'soft-orange': 'hsl(43, 84%, 65%)',
+				'very-dark-blue': 'hsl(226, 43%, 10%)',
+				'dark-blue': 'hsl(235, 46%, 20%)',
+				'soft-dark-blue': 'hsl(235, 50%, 35%)',
+				'desaturated-blue': 'hsl(235, 45%, 61%)',
+				'pale-blue': 'hsl(236, 100%, 87%)',
+			},
+		},
+	},
+	plugins: [],
 }
